6d4eed74044bc593a74a68a488c1840476fde46b
Farouk-Zaazaa <faroukhossam004@gmail.com>
2024-08-08 20:59:25 +0300

    new

---
+++ docs--base-href/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-JDKOP3KY.mjs";import"./chunk-LBP4MM4Y.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { TabletComponent } from './tablet/tablet.component';
+import { IpadComponent } from './ipad/ipad.component';
+import { DesktopComponent } from './desktop/desktop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "" , redirectTo: "home" , pathMatch : "full"},
+
+    {path: "home" , component: HomeComponent},
+    {path: "contact" , component: ContactComponent},
+    {path : "about" , component: AboutComponent , children: [
+        {path: "mobile" , component: MobileComponent},
+        {path: "tablet" , component: TabletComponent},
+        {path: "ipad" , component: IpadComponent},
+        {path: "desktop" , component: DesktopComponent},
+    ]},
+
+    {path: "**" , component : NotfoundComponent},
+];
